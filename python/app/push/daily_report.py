efd24e72bcd3598e4437b5bab350f47f5584ff3e
SinHom <sinhom120625@gmail.com>
2026-06-30 15:34:33 +0800

    chore: 清理 debug print

--- python/app/push/daily_report.py
+++ python/app/push/daily_report.py
@@ -265,14 +265,6 @@ async def _select_weekly_policies(
         if pushed_ids:
             stmt = stmt.where(Policy.id.notin_(pushed_ids))
         policies = list((await session.execute(stmt)).scalars().all())
-    from sqlalchemy.dialects import sqlite as sa_sqlite
-    compiled = str(stmt.compile(dialect=sa_sqlite.dialect(), compile_kwargs={"literal_binds": True}))
-    logger.info("DEBUG _select_weekly SQL: %s", compiled[:500])
-    logger.info("DEBUG _select_weekly: candidate_cutoff=%r, pushed_ids=%s, returned %d policies",
-                candidate_cutoff, list(pushed_ids)[:5], len(policies))
-    for p in policies[:5]:
-        logger.info("DEBUG   policy id=%d crawled_at=%r src=%d summary=%s",
-                    p.id, p.crawled_at, p.source_id, bool(p.summary_text))
 
     # 3) 客户端过滤 region + department(必须在 session 内,统一做)
     src_ids_needed = {p.source_id for p in policies if p.source_id}
